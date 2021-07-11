9f1f501d4f59d86a08d63f3a814d411e53f7c9fe
AfekC <afek8260@gmail.com>
2021-07-11 21:18:06 +0300

    .

---
+++ fileslibrary/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-row class="text-center" style="padding-top: 15vh">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.png')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to The Shared Library
+        </h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
